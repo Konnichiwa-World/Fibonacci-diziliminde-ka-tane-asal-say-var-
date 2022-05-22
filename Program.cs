4bd4b196fabb50532c67192e00c25afa95cab4f1
Onur MUTLU <103672350+Konnichiwa-World@users.noreply.github.com>
2022-05-22 21:55:48 +0300

    Fibonacci sayı diziliminde kaç tane asal sayı var
    "Fibonacci sayı diziliminde kaç tane asal sayı var" sorusunu cevaplayan yazılım.Eğer 1.000.000 gibi yüksek sayı girilirse, işlemcinizin hızına bağlı olarak beklersiniz.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+class program
+{
+    public static void Main(string[] args)
+    { a1:  System.Console.WriteLine("Lütfen sayi giriniz:");
+        Int32 sayi =Int32.Parse(Console.ReadLine());
+
+            ArrayList fibo = new ArrayList(); fibo.Add(1); fibo.Add(1);
+            for(int a=0;a<sayi-2;a++){
+                fibo.Add(Convert.ToInt32(fibo[a+1])+Convert.ToInt32(fibo[a]));}
+            System.Console.WriteLine(" ");
+        
+            int sayac;
+            for (int i = 2; i < sayi; i++)
+            {
+                sayac = 0;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sayac++;
+                    }
+                }
+                if (sayac == 0)
+                {
+                    if(fibo.Contains(i)==true){
+                        System.Console.WriteLine(i);
+                    }
+                }
+            }
+            goto a1;}
+}
